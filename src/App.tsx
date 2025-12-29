fd9f7085cb55b63a80b7f3f1704b908e4783f490
Nahuel Lemes <nahulem@gmail.com>
2025-12-29 08:29:56 -0300

    feat: install and implement react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { Projects } from "./pages/Projects"
+import { Route, Routes } from 'react-router-dom';
+import { Projects } from './pages/Projects';
+import { Home } from './pages/Home';
+import { Navbar } from './components/layout/Navbar';
 
 function App() {
   return (
-    <div className="min-h-screen bg-zinc-950 text-zinc-100">
-      <Projects />
-    </div>
-  )
+    <>
+    <header className="bg-zinc-900 text-white">
+      <p className="p-2 text-center text-sm bg-amber-400 text-zinc-900">
+        🚧 This portfolio is currently under construction! 🚧
+      </p>
+      <Navbar />
+    </header>
+      <main className="p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
