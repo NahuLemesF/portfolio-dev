f72b8f8d9e8af736c4d407fd953898b5cb46c4c3
Nahuel Lemes <nahulem@gmail.com>
2025-12-29 08:29:04 -0300

    feat: create Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+
+export const Home = () => {
+  return (
+    <>
+      <h1>Nahuel Lemes</h1>
+      <h2>Frontend / Fullstack Developer</h2>
+      <p>
+        Welcome to my portfolio! Here you can check out my projects and learn a
+        bit about me. Enjoy your visit!
+      </p>
+    </>
+  );
+};
